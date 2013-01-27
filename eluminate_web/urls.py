e5c7e66f00f4fd125fb48e3469540cdeac93250f
KimSJ <kim@spencejones.com>
2013-01-27 12:14:03 +0000

    added placeholders for all nav menu pages

--- eluminate_web/urls.py
+++ eluminate_web/urls.py
@@ -10,7 +10,11 @@ admin.autodiscover()
 
 urlpatterns = patterns("",
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
+
+    url(r'^exhibition/$', TemplateView.as_view(template_name="exhibition.html"), name="exhibition"),
+    url(r'^sponsors/$', TemplateView.as_view(template_name="sponsors.html"), name="sponsors"),
     url(r'^about/$', TemplateView.as_view(template_name="about.html"), name="about"),
+    url(r'^contact/$', TemplateView.as_view(template_name="contact.html"), name="contact"),
     url(r"^account/", include("account.urls")),
     url(r"^admin/", include(admin.site.urls)),
     url(r"^events/", include("events.urls")),  
