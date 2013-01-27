11e48006fcb7de9e4c59d3bd098e93ee173980d6
Phil Cowans <phil@philcowans.com>
2013-01-27 14:07:23 +0000

    Basic wiring, co functionality yet

--- search/views.py
+++ search/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(["GET"])
+def index(request):
+    return render(request, 'search/index.html')
