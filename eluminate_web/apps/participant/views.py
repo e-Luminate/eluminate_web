d2ca14c22d4d055f01e2cfe2caeb35d419e8a992
jogwen <joanna.wakeford+github@gmail.com>
2013-01-28 09:58:17 +0000

    Created virtual exhibition landing page.

--- eluminate_web/apps/participant/views.py
+++ eluminate_web/apps/participant/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.views.generic.base import TemplateView
+
+from participant.models import Category, Participant
+
+class ParticipantBaseView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(ParticipantBaseView, self).get_context_data(**kwargs)
+        context['participant_list'] = Participant.objects.all().order_by('name')
+        context['category_list'] = Category.objects.all()
+        return context
+
+class ParticipantLandingView(ParticipantBaseView):
+    template_name = 'participant/participant_landing.html'
+
