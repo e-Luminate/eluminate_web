c47aa17bceff81880e6c630ecd36e32f668db468
Michele Mattioni <mattions@gmail.com>
2013-01-29 23:05:38 +0000

    Implemented the days as Many To Many Field, added fixture to preload our day of interest.

--- eluminate_web/apps/events/models.py
+++ eluminate_web/apps/events/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from participant.models import Participant
 
-DAYS = (
-        ("We", "20th Wednesday"),
-        ("Tu", "21rs Thursday"),
-        ("Fr", "22nd Friday"),
-        ("Sa", "23rd Saturday"),
-        ("Su", "24th Sunday")
-        )
+from django.db import models
+from django import forms
+from south.modelsinspector import add_introspection_rules
+
+
+class Day(models.Model):
+    name = models.CharField(max_length = 200)
+    
+    def __str__(self):
+        
+        return self.name
 
 class Event(models.Model):
     name = models.CharField(max_length = 256)
     start_time = models.TimeField()
     end_time = models.TimeField()
-    days = models.CharField(max_length=2,
-                            choices=DAYS,
-                            default=None
-                            )
+    days = models.ManyToManyField(Day)
     participant = models.ForeignKey(Participant)
     description = models.TextField()
