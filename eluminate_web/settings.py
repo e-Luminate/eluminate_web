aa332454c7b9dd5eb9530c8a698f4840de0d5ae3
Michele Mattioni <mattions@gmail.com>
2013-02-01 17:43:07 +0000

    Enabling Django Toolbar and added to the reqs.

--- eluminate_web/settings.py
+++ eluminate_web/settings.py
@@ -116,6 +116,7 @@ MIDDLEWARE_CLASSES = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware"
 ]
 
 ROOT_URLCONF = "eluminate_web.urls"
@@ -149,6 +150,7 @@ INSTALLED_APPS = [
     "eventlog",
     "south",
     "crispy_forms",
+    "debug_toolbar",
     
     # project
     "eluminate_web",
