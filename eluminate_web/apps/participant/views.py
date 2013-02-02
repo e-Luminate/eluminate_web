4b18e71f00650bac04a0c7f4884edda716355d25
jogwen <joanna.wakeford+github@gmail.com>
2013-02-02 18:35:33 +0000

    Implement category filtering.

--- eluminate_web/apps/participant/views.py
+++ eluminate_web/apps/participant/views.py
@@ -6,10 +6,19 @@ from participant.models import Category, Participant
 class ParticipantLandingView(TemplateView):
     template_name = 'participant/participant_landing.html'
 
+    def get(self, request, *args, **kwargs):
+        self.selected_category_id = int(request.GET.get('category', '0'))
+        return super(ParticipantLandingView, self).get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(ParticipantLandingView, self).get_context_data(**kwargs)
-        context['participant_list'] = Participant.objects.all().order_by('name')
-        context['category_list'] = Category.objects.all()
+        context['selected_category_id'] = self.selected_category_id
+        participant_list = Participant.objects.all()
+        if self.selected_category_id: 
+            participant_list = participant_list.filter(category__id=self.selected_category_id)
+        context['participant_list'] = participant_list.order_by('name')
+        used_category_ids = Participant.objects.distinct('category').values_list('category_id', flat=True)
+        context['category_list'] = Category.objects.filter(id__in=used_category_ids).order_by('-id')
         return context
 
 class ParticipantDetailView(DetailView):
@@ -17,9 +26,18 @@ class ParticipantDetailView(DetailView):
     model = Participant
     context_object_name = 'current_participant'
 
+    def get(self, request, *args, **kwargs):
+        self.selected_category_id = int(request.GET.get('category', '0'))
+        return super(ParticipantDetailView, self).get(request, *args, **kwargs)
+
     def get_context_data(self, **kwargs):
         context = super(ParticipantDetailView, self).get_context_data(**kwargs)
-        context['participant_list'] = Participant.objects.all().order_by('name')
-        context['category_list'] = Category.objects.all()
+        context['selected_category_id'] = self.selected_category_id
+        participant_list = Participant.objects.all()
+        if self.selected_category_id: 
+            participant_list = participant_list.filter(category__id=self.selected_category_id)
+        context['participant_list'] = participant_list.order_by('name')
+        used_category_ids = Participant.objects.distinct('category').values_list('category_id', flat=True)
+        context['category_list'] = Category.objects.filter(id__in=used_category_ids).order_by('-id')
         return context
 
