913caf3b581233351edc217f4ed237794342db00
jogwen <joanna.wakeford+github@gmail.com>
2013-02-02 20:04:04 +0000

    Implemented a new RedirectView which redirects category filter requests to the detail page of the first matching participant, or to the landing page if none, preserving the selected category in both cases.

--- eluminate_web/apps/participant/views.py
+++ eluminate_web/apps/participant/views.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+from django.views.generic import RedirectView
 from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 
@@ -7,15 +9,21 @@ class ParticipantMixin(object):
     def set_selected_category(self, request):
         self.selected_category_id = int(request.GET.get('category', '0'))
 
-    def get_participant_context_data(self):
-        context = {}
-        context['selected_category_id'] = self.selected_category_id
+    def get_participant_list(self):
         participant_list = Participant.objects.all()
         if self.selected_category_id: 
             participant_list = participant_list.filter(category__id=self.selected_category_id)
-        context['participant_list'] = participant_list.order_by('name')
-        used_category_ids = Participant.objects.distinct('category').values_list('category_id', flat=True)
-        context['category_list'] = Category.objects.filter(id__in=used_category_ids).order_by('-id')
+        return participant_list.order_by('name')
+
+    def get_active_category_list(self):
+        active_category_ids = Participant.objects.distinct('category').values_list('category_id', flat=True)
+        return Category.objects.filter(id__in=active_category_ids).order_by('-id')
+
+    def get_participant_context_data(self):
+        context = {}
+        context['selected_category_id'] = self.selected_category_id
+        context['participant_list'] = self.get_participant_list()
+        context['category_list'] = self.get_active_category_list()
         return context
 
 class ParticipantLandingView(TemplateView, ParticipantMixin):
@@ -43,3 +51,19 @@ class ParticipantDetailView(DetailView, ParticipantMixin):
         context = super(ParticipantDetailView, self).get_context_data(**kwargs)
         context.update(self.get_participant_context_data())
         return context
+
+class ParticipantFilterRedirectView(RedirectView, ParticipantMixin):
+    url = 'X'
+    query_string = True
+
+    def get(self, request, *args, **kwargs):
+        self.set_selected_category(request)
+        return super(ParticipantFilterRedirectView, self).get(request, *args, **kwargs)
+
+    def get_redirect_url(self, **kwargs):
+        query_string = super(ParticipantFilterRedirectView, self).get_redirect_url(**kwargs)[len(self.url):]
+        participants = self.get_participant_list()
+        if participants:
+            return reverse('participant_detail', kwargs={'slug': participants[0].slug}) + query_string
+        else:
+            return reverse('participant_landing') + query_string
