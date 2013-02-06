abcb0d402b8ef2f57b03ffc6053a08550396d558
Michele Mattioni <mattions@gmail.com>
2013-02-06 11:41:20 +0000

    removed not needed directories.

--- search/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from search import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name = 'root'),
-)
-
--- search/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.http import require_http_methods
-
-@require_http_methods(["GET"])
-def index(request):
-    return render(request, 'search/index.html')
