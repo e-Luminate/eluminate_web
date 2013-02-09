4426a7b45510e12d8abfdea4dec45b404cdf3596
jogwen <joanna.wakeford+github@gmail.com>
2013-02-09 18:48:43 +0000

    Removed unecessary import

--- eluminate_web/apps/custom_account/views.py
+++ eluminate_web/apps/custom_account/views.py
@@ -1,4 +1,4 @@
-from django.contrib import auth, messages
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as _
