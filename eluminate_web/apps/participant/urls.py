b8b9fe271ce3d9e26e8b582c6ebd511a65ce1f12
Michele Mattioni <mattions@gmail.com>
2013-02-10 17:47:11 +0000

    removing the details from the url. Fix #126

--- eluminate_web/apps/participant/urls.py
+++ eluminate_web/apps/participant/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('participant.views',
     url(r'^$', ParticipantLandingView.as_view(), name='participant_landing'),
     url(r'^create$', ParticipantCreateView.as_view(), name="participant_create"),
     url(r'^filter/$', ParticipantFilterRedirectView.as_view(), name='participant_filter'),
-    url(r'^detail/(?P<slug>[a-z0-9_\-]+)/$', ParticipantDetailView.as_view(), name='participant_detail'),
+    url(r'^(?P<slug>[a-z0-9_\-]+)/$', ParticipantDetailView.as_view(), name='participant_detail'),
     url(r'^(?P<slug>[a-z0-9_\-]+)/update/$', ParticipantUpdateView.as_view(), name="participant_update"),
     url(r'^(?P<slug>[a-z0-9_\-]+)/delete/$', ParticipantDeleteView.as_view(), name="participant_delete")
 )
