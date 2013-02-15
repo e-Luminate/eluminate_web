b5b7e80195f70536ee23958f388321c34483e099
Kim SJ <kim@spencejones.com>
2013-02-15 15:09:07 +0100

    Fixed wsgi virtualenv activation

--- eluminate_web/wsgi.py
+++ eluminate_web/wsgi.py
@@ -23,9 +23,9 @@ try:
 except IOError:
     pass
 
-# activate virtualenv (may be needed on production server; not sure)
-#activate_this = os.path.expanduser("~/.virtualenvs/eluminate_env/bin/activate_this.py")
-#execfile(activate_this, dict(__file__=activate_this))
+# activate virtualenv (may be needed on production server; judging by today's effort, it is!)
+activate_this = os.path.expanduser("~/.virtualenvs/eluminate_env/bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
 
 import eluminate_web.startup as startup
 startup.run()
