2679351b89dbcca6b72df1db4b907a9e0dbef06c
KimSJ <kim@spencejones.com>
2014-01-22 18:39:03 +0000

    working new dates

---
+++ eluminate_web/apps/events/templatetags/date_tidy.py
@@ -0,0 +1,33 @@
+from django import template
+from events.models import Day
+
+register = template.Library()
+
+@register.filter(name='tidy')
+def tidy(theDaysObj):
+    """Compresses dates into a minimal string"""
+
+    # make an index of days
+    allDays = list(Day.objects.all().order_by('pk'))
+    #return type(theDays.all())
+    theDays = list(theDaysObj.all())
+
+    s = str(theDays[0])             # the first (and maybe the only) day in the list
+    if len(theDays) == 2:      # only two days, so comma list them
+        s += ", " + str(theDays[1])
+    if len(theDays)>2:         # more than two days, so look for ranges we can replace with a hyphen
+        for t in range(1,len(theDays)-1):
+            i = allDays.index(theDays[t]) 
+            if ((allDays.index(theDays[t-1]) == i-1) and (allDays.index(theDays[t+1]) == i+1)):
+                # it is in the middle of a range...
+                if s[-2:] != "- ": # if it's the first day of a range, put in the hypehn
+                    s += " - "
+            else: # it's either the start/end of a range, or a one-off
+                if s[-2:] != "- ": # it's the start or a one-off -- needs a comma in both cases
+                    s += ", "
+                s += str(theDays[t]) # and in all three cases, we need the date itself.
+        if s[-2:] != "- ":
+            s = s + ", "
+        s +=  str(theDays[-1])
+    return s
+
