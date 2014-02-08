1fc8631b19e54fedb512a0adfd50671e2c69dcd5
Kim SJ <kim@spencejones.com>
2014-02-08 19:01:38 +0000

    Event sort order code tidy

--- eluminate_web/apps/events/admin.py
+++ eluminate_web/apps/events/admin.py
@@ -4,9 +4,9 @@ from events.models import Event, Day
 class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'participant', 'priority', 'isEvent', 'featured')
     list_editable = ('priority', 'isEvent', 'featured')
-    filter_horizontal = ('days',)
     ordering = ('-featured','-isEvent','-priority')
-    pass
+
+    filter_horizontal = ('days',)
 
 admin.site.register(Event, EventAdmin)
 
