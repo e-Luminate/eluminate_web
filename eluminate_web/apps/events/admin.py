3d82874db5bc3a8210674c7c205159367970ecfc
Kim SJ <kim@spencejones.com>
2014-02-08 18:44:32 +0000

    Event sort order introduced

--- eluminate_web/apps/events/admin.py
+++ eluminate_web/apps/events/admin.py
@@ -5,6 +5,7 @@ class EventAdmin(admin.ModelAdmin):
     list_display = ('name', 'participant', 'priority', 'isEvent', 'featured')
     list_editable = ('priority', 'isEvent', 'featured')
     filter_horizontal = ('days',)
+    ordering = ('-featured','-isEvent','-priority')
     pass
 
 admin.site.register(Event, EventAdmin)
